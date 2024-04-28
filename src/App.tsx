03ccaf6e877d82a5a7c9511e31d33cbd4819a494
ToavinaMaria <ToavinaMaria>
2024-04-29 01:20:36 +0300

    fix: App.tsx and tsconfig.json

--- src/App.tsx
+++ src/App.tsx
@@ -6,5 +6,23 @@ import {
   ShowGuesser,
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
+import { PostList } from "./posts/PostList";
+import { UserList } from "./posts/UserList";
 
-export const App = () => <Admin dataProvider={dataProvider}></Admin>;
+export const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="users"
+      list={UserList}
+      recordRepresentation={"name"}
+      edit={EditGuesser}
+      show={ShowGuesser}
+    />
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={EditGuesser}
+      show={ShowGuesser}
+    />
+  </Admin>
+);
