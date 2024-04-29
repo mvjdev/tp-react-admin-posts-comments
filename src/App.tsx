146685ad30a58518246e56f81fe9b00aac660c89
ToavinaMaria <ToavinaMaria>
2024-04-29 07:39:43 +0300

    fix: App.tsx and index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import {
-  Admin,
-  Resource,
-  ListGuesser,
-  EditGuesser,
-  ShowGuesser,
-} from "react-admin";
-import { dataProvider } from "./dataProvider";
-import { PostList } from "./posts/PostList";
-import { UserList } from "./posts/UserList";
+import * as React from "react";
+import { Admin, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+import { PostList, PostEdit, PostCreate, PostShow } from "./posts/Posts";
+import { CommentList, CommentEdit, CommentCreate } from "./comments/Comments";
 
-export const App = () => (
+const jsonServerUrl =
+  process.env.VITE_JSON_SERVER_URL || "https://jsonplaceholder.typicode.com";
+
+const dataProvider = jsonServerProvider(jsonServerUrl);
+
+const App: React.FC = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource
-      name="users"
-      list={UserList}
-      recordRepresentation={"name"}
-      edit={EditGuesser}
-      show={ShowGuesser}
-    />
     <Resource
       name="posts"
       list={PostList}
-      edit={EditGuesser}
-      show={ShowGuesser}
+      edit={PostEdit}
+      create={PostCreate}
+      show={PostShow}
+    />
+    <Resource
+      name="comments"
+      list={CommentList}
+      edit={CommentEdit}
+      create={CommentCreate}
     />
   </Admin>
 );
+
+export default App;
